b187536e31acd626ae4e46fa22f93ed2747a918f
tanisha-1909 <tanishamahavar32@gmail.com>
2025-04-02 15:38:11 +0530

    route for all hotels and specific hotel info

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express= require('express');
 const app= express();
 const mongoose= require('mongoose');
 const Listing= require("./models/listing.js")
+const path= require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/stayease');
@@ -19,15 +24,26 @@ app.get("/", (req,res)=>{
     res.send("I am root");
 })
 
-app.get("/testListing", async (req,res)=>{
-    let sampleListing= new Listing({
-        title:"My Villa",
-        description:"Off shore",
-        price:1200,
-        location:"Alwar, Rajasthan",
-        country:"India"
-    });
-    await sampleListing.save();
-    console.log(sampleListing);
-    res.send("working");
+app.get("/listings", async (req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("./listings/allList.ejs",{allListings})
 })
+
+app.get("/listings/:id", async(req,res)=>{
+    let {id}= req.params;
+    const listing=  await Listing.findById(id);
+    res.render("./listings/show.ejs", {listing});
+})
+
+// app.get("/testListing", async (req,res)=>{
+//     let sampleListing= new Listing({
+//         title:"My Villa",
+//         description:"Off shore",
+//         price:1200,
+//         location:"Alwar, Rajasthan",
+//         country:"India"
+//     });
+//     await sampleListing.save();
+//     console.log(sampleListing);
+//     res.send("working");
+// })
