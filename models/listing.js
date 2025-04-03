b030024704b72a2b2e8fe85d9d42aea25339bd53
tanisha-1909 <tanishamahavar32@gmail.com>
2025-04-03 23:44:26 +0530

    routes styling done

--- models/listing.js
+++ models/listing.js
@@ -12,7 +12,8 @@ const listingSchema= new mongoose.Schema({
         filename: String,
         url: {
             type: String,
-            set: (v) => v === "" ? "https://unsplash.com/photos/coconut-palm-trees-in-hotel-lobby-_dS27XGgRyQ" : v
+            default: "https://images.unsplash.com/photo-1568495248636-6432b97bd949?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww",
+            set: (v) => v && v.trim() !== "" ? v : "https://images.unsplash.com/photo-1568495248636-6432b97bd949?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww"
         }
     },
     price:String,
